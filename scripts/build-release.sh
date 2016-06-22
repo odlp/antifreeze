821e56b744ad448a56916977a30eac54ae0e6e19
odlp <oliverp@gmail.com>
2016-06-22 11:56:57 +0800

    Update build release script to print SHA-1 digests [ci-skip]

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -4,18 +4,19 @@ set -e
 
 SANDBOX=$(mktemp -d)
 
-echo "Building Linux..."
+printf "Building Linux...\n"
 GOOS=linux go build -o $SANDBOX/antifreeze-linux github.com/odlp/antifreeze
 
-echo "Building OSX..."
+printf "Building OSX...\n"
 GOOS=darwin go build -o $SANDBOX/antifreeze-darwin github.com/odlp/antifreeze
 
-echo "Building Windows..."
+printf "Building Windows...\n"
 GOOS=windows go build -o $SANDBOX/antifreeze.exe github.com/odlp/antifreeze
 
-echo
-
+printf "\nBuild summary:\n"
 find $SANDBOX -type f -exec file {} \;
 
-echo
-echo "Binaries are in: $SANDBOX"
+printf "\nSHA-1 digests for CF cli plugin repo:\n"
+shasum $SANDBOX/antifreeze*
+
+printf "\nBinaries are located here:\n$SANDBOX\n"
